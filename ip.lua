db93a93d267bcc1e0d9a0b00ffbde32a82970efd
Sean Conner <spc@conman.org>
2013-04-24 21:14:51 -0400

    Convert into a usable module.

--- ip.lua
+++ ip.lua
@@ -1,6 +1,28 @@
+-- ***************************************************************
+--
+-- Copyright 2013 by Sean Conner.  All Rights Reserved.
+-- 
+-- This library is free software; you can redistribute it and/or modify it
+-- under the terms of the GNU Lesser General Public License as published by
+-- the Free Software Foundation; either version 3 of the License, or (at your
+-- option) any later version.
+-- 
+-- This library is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+-- License for more details.
+-- 
+-- You should have received a copy of the GNU Lesser General Public License
+-- along with this library; if not, see <http://www.gnu.org/licenses/>.
+--
+-- Comments, questions and criticisms can be sent to: sean@conman.org
+--
+-- ********************************************************************
 
-local idiv = require "org.conman.math".idiv
-local lpeg = require "lpeg"
+local floor    = math.floor
+local string   = string
+local tonumber = tonumber
+local lpeg     = require "lpeg"
 
 local Cf = lpeg.Cf
 local Cc = lpeg.Cc
@@ -8,46 +30,48 @@ local C  = lpeg.C
 local R  = lpeg.R
 local P  = lpeg.P
 
-function acc(a,v) return a .. v end
+module(...)
 
-DIGIT  = R"09"
-HEXDIG = R("AF","af","09")
+-- *********************************************************************
 
-dec_octet = C(DIGIT^1)
-          / function(c)
-              local n = tonumber(c)
-              if n < 256 then
-                return string.char(n)
-              end
-            end
-              
+local function acc(a,v) return a .. v end
+
+local DIGIT  = R"09"
+local HEXDIG = R("AF","af","09")
+
+local dec_octet = C(DIGIT^1)
+                / function(c)
+                    local n = tonumber(c)
+                    if n < 256 then
+                      return string.char(n)
+                    end
+                  end
+
 IPv4 = Cf(
-           Cc"" 
-           * dec_octet * "." 
-           * dec_octet * "." 
-           * dec_octet * "." 
-           * dec_octet,
-           acc)
+	Cc"" * dec_octet * "." * dec_octet * "." * dec_octet * "." * dec_octet,
+	acc
+)
 
-cc   = P"::" / "\0\0"
-set  = -P(R("AF","af","09","::"))
-h16  = C(HEXDIG^1)
-     / function(c)
-         local n = tonumber(c,16)
-         if n < 65536 then
-           local q,r = idiv(n,256)
-           return string.char(q) .. string.char(r)
-         end
-       end
+local cc   = P"::" / "\0\0"
+local set  = -P(R("AF","af","09","::"))
+local h16  = C(HEXDIG^1)
+           / function(c)
+               local n = tonumber(c,16)
+               if n < 65536 then
+                 local q = floor(n/256)
+                 local r = n % 256
+                 return string.char(q) .. string.char(r)
+               end
+             end
 
-h16c = h16 * P":" * #HEXDIG
-ls32 = IPv4 + h16c * h16
+local h16c = h16 * P":" * #HEXDIG
+local ls32 = IPv4 + h16c * h16
 
-function mcc(n)
+local function mcc(n)
   return P"::" / string.rep("\0",n * 2)
 end
 
-function mh16(n)
+local function mh16(n)
   local acc = P(true)
   for i = 1 , n-1 do
     acc = acc * h16c
@@ -56,7 +80,7 @@ function mh16(n)
   return acc
 end
 
-function mh16c(n)
+local function mh16c(n)
   local acc = P(true)
   for i = 1 , n do
     acc = acc * h16c
@@ -93,7 +117,7 @@ IPv6 = Cf(Cc"" *                    mh16c(6) * ls32 * set,acc)	-- a
      + Cf(Cc"" * mh16(4) * mcc(3)            * h16  * set,acc)
      + Cf(Cc"" * mh16(5) * mcc(2)            * h16  * set,acc)
      + Cf(Cc"" * mh16(6) * mcc(1)            * h16  * set,acc)
-     + Cf(Cc"" *           mcc(8)                   * set,acc)		-- i
+     + Cf(Cc"" *           mcc(8)                   * set,acc)	-- i
      + Cf(Cc"" * mh16(1) * mcc(7)                   * set,acc)
      + Cf(Cc"" * mh16(2) * mcc(6)                   * set,acc)
      + Cf(Cc"" * mh16(3) * mcc(5)                   * set,acc)
@@ -102,68 +126,4 @@ IPv6 = Cf(Cc"" *                    mh16c(6) * ls32 * set,acc)	-- a
      + Cf(Cc"" * mh16(6) * mcc(2)                   * set,acc)
      + Cf(Cc"" * mh16(7) * mcc(1)                   * set,acc)
      
--- -------------------------------------------------------------
 
-
-
-
-
-ddt = require "org.conman.debug"
-
-function test(f,addr)
-  local x = f:match(addr)
-  if x then
-    ddt.hexdump(x)
-  else
-    print("FAIL","(nil)",addr)
-  end
-end
-
-
-test(IPv4,"10.10.10.10")
-test(IPv4,"0.0.0.0")
-test(IPv4,"255.255.255.255")
-test(IPv4,"10.256,10.10")
-
-test(IPv6,"1111:2222:3333:4444:5555:6666:10.10.10.10")
-test(IPv6,"1111:2222:3333:4444:5555:6666:7777:8888")
-test(IPv6,"fc00::1")
-test(IPv6,"fc00::dead:beef")
-test(IPv6,"1:2:3:4:5:6:7:8")	-- a
-test(IPv6,"::2:3:4:5:6:7:8")	-- b
-test(IPv6, "::3:4:5:6:7:8")	-- c
-test(IPv6,"1::3:4:5:6:7:8")	-- c
-test(IPv6,   "::4:5:6:7:8")	-- d
-test(IPv6,  "1::4:5:6:7:8")	-- d
-test(IPv6,"1:2::4:5:6:7:8")	-- d
-test(IPv6,     "::5:6:7:8")	-- e
-test(IPv6,    "1::5:6:7:8")	-- e
-test(IPv6,  "1:2::5:6:7:8")	-- e
-test(IPv6,"1:2:3::5:6:7:8")	-- e
-test(IPv6,       "::6:7:8")	-- f
-test(IPv6,      "1::6:7:8")	-- f
-test(IPv6,    "1:2::6:7:8")	-- f
-test(IPv6,  "1:2:3::6:7:8")	-- f
-test(IPv6,"1:2:3:4::6:7:8")	-- f
-test(IPv6,         "::7:8")	-- g
-test(IPv6, "::10.10.10.10")	-- g
-test(IPv6,        "1::7:8")	-- g
-test(IPv6,      "1:2::7:8")	-- g
-test(IPv6,    "1:2:3::7:8")	-- g
-test(IPv6,  "1:2:3:4::7:8")	-- g
-test(IPv6,"1:2:3:4:5::7:8")	-- g
-test(IPv6,           "::8")	-- h
-test(IPv6,          "1::8")	-- h
-test(IPv6,        "1:2::8")	-- h
-test(IPv6,      "1:2:3::8")	-- h
-test(IPv6,    "1:2:3:4::8")	-- h
-test(IPv6,  "1:2:3:4:5::8")	-- h
-test(IPv6,"1:2:3:4:5:6::8")	-- h
-test(IPv6,             "::")	-- i
-test(IPv6,            "1::")	-- i
-test(IPv6,          "1:2::")	-- i
-test(IPv6,        "1:2:3::")	-- i
-test(IPv6,      "1:2:3:4::")	-- i
-test(IPv6,    "1:2:3:4:5::")	-- i
-test(IPv6,  "1:2:3:4:5:6::")	-- i
-test(IPv6,"1:2:3:4:5:6:7::")	-- i
