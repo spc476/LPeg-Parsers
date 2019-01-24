724eac2c5b327dec007c7dd3d148f4e90cc2102c
Sean Conner <spc@conman.org>
2019-01-24 01:10:36 -0500

    Add parser for UTF-8
    This will parse one (1) UTF-8 codepoint, EXCLUDING C0 and C1 control sets.

---
+++ utf8.lua
@@ -0,0 +1,46 @@
+-- ***************************************************************
+--
+-- Copyright 2016 by Sean Conner.  All Rights Reserved.
+--
+-- This library is free software; you can redistribute it and/or modify it
+-- under the terms of the GNU Lesser General Public License as published by
+-- the Free Software Foundation; either version 3 of the License, or (at your
+-- option) any later version.
+--
+-- This library is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+-- License for more details.
+--
+-- You should have received a copy of the GNU Lesser General Public License
+-- along with this library; if not, see <http://www.gnu.org/licenses/>.
+--
+-- Comments, questions and criticisms can be sent to: sean@conman.org
+--
+-- ====================================================================
+--
+-- Validate UTF-8 characters via LPeg.  This will *exclude* all C0
+-- and C1 control characters.
+--
+-- ********************************************************************
+-- luacheck: ignore 611
+-- RFC-3629
+--
+-- NOTE: This excludes the C0 and C1 control sets.
+--
+--       Characters 192-193 and 245-255 will never appear in proper UTF-8
+--       encoding.
+
+local lpeg = require "lpeg"
+
+return lpeg.R" ~"
+     + lpeg.P"\194"     * lpeg.R"\160\191"
+     + lpeg.R"\195\223" * lpeg.R"\128\191"
+     + lpeg.P"\224"     * lpeg.R"\160\191" * lpeg.R"\128\191"
+     + lpeg.R"\225\236" * lpeg.R"\128\191" * lpeg.R"\128\191"
+     + lpeg.P"\237"     * lpeg.R"\128\159" * lpeg.R"\128\191"
+     + lpeg.R"\238\239" * lpeg.R"\128\191" * lpeg.R"\128\191"
+     + lpeg.P"\240"     * lpeg.R"\144\191" * lpeg.R"\128\191" * lpeg.R"\128\191"
+     + lpeg.R"\241\243" * lpeg.R"\128\191" * lpeg.R"\128\191" * lpeg.R"\128\191"
+     + lpeg.P"\244"     * lpeg.R"\128\143" * lpeg.R"\128\191" * lpeg.R"\128\191"
+     
