fc702de05b2c2a0dc973e0f720e54f5e64295c9c
Sean Conner <spc@conman.org>
2019-08-29 00:18:08 -0400

    Major reworking of the data: URI parser
    I re-read RFC-2397, and after working on a mimetype parser, decided to
    rewrite this parser.

--- url/data.lua
+++ url/data.lua
@@ -24,78 +24,73 @@
 local abnf = require "org.conman.parsers.abnf"
 local lpeg = require "lpeg"
 
+local Cc = lpeg.Cc
+local Cf = lpeg.Cf
+local Cg = lpeg.Cg
+local Cs = lpeg.Cs
+local Ct = lpeg.Ct
+local C  = lpeg.C
+local P  = lpeg.P
+local R  = lpeg.R
+local S  = lpeg.S
+
 -- ********************************************************************
 
-local CHARSET    = (lpeg.P"C" + lpeg.P"c" + lpeg.P"%43" + lpeg.P"%63")
-                 * (lpeg.P"H" + lpeg.P"h" + lpeg.P"%48" + lpeg.P"%68")
-                 * (lpeg.P"A" + lpeg.P"a" + lpeg.P"%41" + lpeg.P"%61")
-                 * (lpeg.P"R" + lpeg.P"r" + lpeg.P"%52" + lpeg.P"%72")
-                 * (lpeg.P"S" + lpeg.P"s" + lpeg.P"%53" + lpeg.P"%73")
-                 * (lpeg.P"E" + lpeg.P"e" + lpeg.P"%45" + lpeg.P"%65")
-                 * (lpeg.P"T" + lpeg.P"t" + lpeg.P"%54" + lpeg.P"%74")
-local BASE64     = (lpeg.P"b" + lpeg.P"%62")
-                 * (lpeg.P"a" + lpeg.P"%61")
-                 * (lpeg.P"s" + lpeg.P"%73")
-                 * (lpeg.P"e" + lpeg.P"%65")
-                 * (lpeg.P"6" + lpeg.P"%36")
-                 * (lpeg.P"4" + lpeg.P"%34")
-local escaped    = lpeg.P"%" * lpeg.C(abnf.HEXDIG * abnf.HEXDIG)
+local escaped    = P"%" * C(abnf.HEXDIG * abnf.HEXDIG)
                  / function(c) return string.char(tonumber(c,16)) end
-local mark       = lpeg.S"-_.!~*'()"
+local mark       = S"-_.!~*'()"
 local unreserved = abnf.ALPHA + abnf.DIGIT + mark
 local uric       = escaped + unreserved
-local base64     = abnf.ALPHA + abnf.DIGIT + lpeg.S"+/"
-local iana_token = escaped + lpeg.R("AZ","az","09","--","..")
-local majortype  = iana_token^1
-local minortype  = iana_token^1 + lpeg.P"*"
-local mimetype   = majortype * lpeg.P"/" * minortype
-local especials  = lpeg.P"%28" + lpeg.P"%29"
-                 + lpeg.P"%3C" + lpeg.P"%3E" + lpeg.P"%3c" + lpeg.P"%3e"
-                 + lpeg.P"%40"
-                 + lpeg.P"%2C" + lpeg.P"%2c"
-                 + lpeg.P"%3B" + lpeg.P"%3b"
-                 + lpeg.P"%3A" + lpeg.P"%3a"
-                 + lpeg.P"%5C" + lpeg.P"%5c"
-                 + lpeg.P"%22"
-                 + lpeg.P"%2F" + lpeg.P"%2f"
-                 + lpeg.P"%5B" + lpeg.P"%5D" + lpeg.P"%5b" + lpeg.P"%5d"
-                 + lpeg.P"%3F" + lpeg.P"%3f"
-                 + lpeg.P"%3D" + lpeg.P"%3d"
-                 + lpeg.P"%2A" + lpeg.P"%2a"
-                 + lpeg.P"%27"
-                 + lpeg.P"%25"
-local quote      = lpeg.P'"' + lpeg.P"%22"
-local token      = (escaped - especials)
-                 + lpeg.R"!~" - lpeg.S[[()<>@,;:\"/[]?=*'%]] -- RFC-2231
-local attribute  = token^1 / function(c) return c:lower() end
-local qs         = quote
-                 * (-quote * (escaped + lpeg.R"!~"))^1
-                 * quote
-local value      = token^1 + qs
-local parameter  = attribute * lpeg.P"=" * lpeg.C(value)
-local parmlist   = lpeg.Cf(
-                            lpeg.Ct"" * (lpeg.P";" * lpeg.Cg(parameter))^1,
-                            function(acc,n,v)
-                              acc[n] = v
-                              return acc
-                            end
-                          )
-local mediatype  = lpeg.Cg(mimetype,'type')
-                 * lpeg.Cg(#lpeg.P";" * parmlist,'parameters')^-1
-local charset    = lpeg.Cg(lpeg.Ct(CHARSET * lpeg.P"=" * lpeg.Cg(value,'charset')),'parameters')
-local data       = lpeg.Cg(lpeg.P"data:" / "data",'scheme')
-                 * lpeg.Cg(lpeg.Cc('text/plain'),'type')
-                 * lpeg.Cg(lpeg.Ct(lpeg.Cg(lpeg.Cc'US-ASCII','charset')),'parameters')
+
+local base64     = abnf.ALPHA + abnf.DIGIT + S"+/"
+
+-- ------------------------------------------------------------------------
+-- RFC-2045 lists [=["(),/:;<=>?@[\]]=] as tspecials, which cannot be in
+-- a parameter name, and *must* be quoted to appear in a value.
+--
+-- RFC-3986 lists [=[ "#%<>[\]^{|}]=] as unsafe and these MUST always be
+-- escaped in a URL.
+--
+-- If you match up the two lists, then the following list [=[#%^{|}]=] are
+-- the characters that can appear in a parameter name that MUST be escaped.
+-- ------------------------------------------------------------------------
+
+local mcharsafe  = R"AZ" / string.lower
+                 + S[[!$&`+-.0123456789_`abcdefghijklmnopqrstuvwxyz~]]
+local ichar      = mcharsafe
+                 + (P"%%23")           / "#"
+                 + (P"%%25")           / "%%"
+                 + (P"%%5E" + P"%%5e") / "^"
+                 + (P"%%7B" + P"%%7b") / "{"
+                 + (P"%%7C" + P"%%7c") / "|"
+                 + (P"%%7D" + P"%%7d") / "}"
+local char       = escaped + (R"!~" - S";,")
+local itoken     = Cs(ichar^1)
+local token      = Cs(char^1)
+local parameters = Cf(
+                       Ct"" * (P";" * Cg(itoken * P"=" * token))^0,
+                       function(acc,name,val)
+                         acc[name] = val
+                         return acc
+                       end
+                     )
+local mimetype   = Cs(ichar^1 * P"/" * ichar^1)
+local mediatype  = Cg(mimetype,'type')
+                 * Cg(#P";" * parameters,'parameters')^-1
+local charset    = Cg(Ct(P"charset=" * Cg(token,'charset')),'parameters')
+local data       = Cg(P"data:" / "data",'scheme')
+                 * Cg(Cc('text/plain'),'type')
+                 * Cg(Ct(Cg(Cc'US-ASCII','charset')),'parameters')
                  * (mediatype + charset)^-1
                  * (
                      (
-                       lpeg.Cg(lpeg.P";" * BASE64 * lpeg.Cc(true),'base64')
-                       * lpeg.P"," * lpeg.Cg(lpeg.C(base64^1),'data')
+                       Cg(P";base64" * Cc(true),'base64')
+                       * P"," * Cg(C(base64^1),'data')
                      )
                      +
                      (
-                       lpeg.Cg(lpeg.Cc(false),'base64')
-                       * lpeg.P"," * lpeg.Cg(lpeg.C(uric^1),'data')
+                       Cg(Cc(false),'base64')
+                       * P"," * Cg(C(uric^1),'data')
                      )
                    )
-return lpeg.Ct(data)
+return Ct(data)
